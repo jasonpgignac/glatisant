ab7301e07e867610e16271e15c2c18d086c16519
jasonpgignac <jasonpgignac@gmail.com>
2017-07-17 15:59:25 +0000

    Displays issue data

--- source/js/app.jsx
+++ source/js/app.jsx
@@ -5,9 +5,26 @@ import IssuesTable from './components/IssuesTable';
 
 require('../css/app.css');
 
+const issuesData = [
+  {
+    id: 123456,
+    title: 'Bells have been Broken',
+    creator: 'Frankie',
+    status: 'open',
+    summary: 'How nobly did the bells ring that day! Their copper clappers drumming, they played wild and tuneless, as if driven by the wind - though the wind, here, was the hunchback, his arms swinging up and down wildly. Ring! Ring! Ring! And the rope twanged and whirled, and snapped and broke, and down, down the high tower he fell. Down, down to the bottom.',
+  },
+  {
+    id: 34343,
+    title: 'I am sad',
+    creator: 'Jean',
+    status: 'open',
+    summary: 'I was bad, and the bishop will be angry',
+  },
+];
+
 ReactDOM.render(
   <h1>
     <Logo />Welcome to Glatisant
-    <IssuesTable headers={['rat']} />
+    <IssuesTable data={issuesData} />
   </h1>,
   document.getElementById('app'));
--- source/js/components/IssuesTable.jsx
+++ source/js/components/IssuesTable.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function truncateField(field, length) {
+  return field.length <= length ? field
+                                : `${field.substr(0, length).trim()}...`;
+}
+
 class IssuesTable extends React.PureComponent {
 
   render() {
-    return (<table>
-      <thead>
-        <tr>
-          {
-            this.props.headers.map(object => <th key={`${object}-header`}>{object}</th>)
-          }
-        </tr>
-      </thead>
-    </table>);
+    return (
+      <div className="issues">
+        {
+          this.props.data.map(issue => (
+            <article key={`Issue-${issue.id}`}>
+              <div className="basic-issue-data">
+                <div>{issue.id}</div>
+                <div>{issue.status}</div>
+                <div>{issue.title}</div>
+                <div>{issue.creator}</div>
+              </div>
+              <div className="issuesummary">{truncateField(issue.summary, 140)}</div>
+            </article>
+          ))
+        }
+      </div>
+    );
   }
-
 }
 
 IssuesTable.propTypes = {
-  headers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    creator: PropTypes.string,
+    status: PropTypes.string,
+    summary: PropTypes.string,
+  })).isRequired,
 };
 
 export default IssuesTable;
