2662de9ddba48cde3d1883dab002a6f53b735f3f
jasonpgignac <jasonpgignac@gmail.com>
2017-07-18 18:42:33 +0000

    Created rough detail page

--- source/js/components/IssuesTable.jsx
+++ source/js/components/IssuesTable.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IssueDetail from './IssueDetail';
 
 function truncateField(field, length) {
   return field.length <= length ? field
                                 : `${field.substr(0, length).trim()}...`;
 }
 
+
 class IssuesTable extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = { selectedIssue: null, showSelectedIssue: false };
+  }
+  openIssue(id) {
+    this.setState({
+      selectedIssue: this.props.data.find(issue => issue.id === id),
+      showSelectedIssue: true,
+    });
+  }
 
   render() {
     return (
       <div className="issues">
+        <IssueDetail issue={this.state.selectedIssue} show={this.state.showSelectedIssue} />
         {
           this.props.data.map(issue => (
             <article key={`Issue-${issue.id}`}>
               <div className="basic-issue-data">
-                <div>{issue.id}</div>
+                <div>
+                  <a
+                    role="link"
+                    onClick={() => this.openIssue(issue.id)}
+                    tabIndex="-1"
+                  >
+                    {issue.id}
+                  </a>
+                </div>
                 <div>{issue.status}</div>
                 <div>{issue.title}</div>
                 <div>{issue.creator}</div>
