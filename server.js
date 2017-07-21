00bb61fd3f20ba8671cc57e95e24187437059199
jasonpgignac <jasonpgignac@gmail.com>
2017-07-21 21:33:49 +0000

    added diagnostic line

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ fs.readdir(p, function (err, files) {
 
     files.map(function (file) {
         return path.join(p, file);
-    }).filter(function (file) {
-        return fs.statSync(file).isFile();
     }).forEach(function (file) {
         console.log("%s (%s)", file, path.extname(file));
     });
