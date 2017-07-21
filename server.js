201b300b81a6e5b104560b1b3a753223a6f49062
jasonpgignac <jasonpgignac@gmail.com>
2017-07-21 21:24:22 +0000

    added diagnostic line

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'); 
 var app = express();
 
+console.log("Serving from", __dirname + '/dist');
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
