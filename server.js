1328076f99618dfdf13681bc0c76d3381313abed
jasonpgignac <jasonpgignac@gmail.com>
2017-07-21 21:28:56 +0000

    added diagnostic line

--- server.js
+++ server.js
@@ -2,5 +2,24 @@ var express = require('express');
 var app = express();
 
 console.log("Serving from", __dirname + '/dist');
+var fs = require("fs"),
+    path = require("path");
+
+var p = __dirname;
+
+fs.readdir(p, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(p, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+      console.log("Here's the files")
+        console.log("%s (%s)", file, path.extname(file));
+    });
+});
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
